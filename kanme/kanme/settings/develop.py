c41804ac1d1b7a14a223b8509db2698533ca779d
tsai <tsai@kanme.net>
2020-07-10 22:28:29 +0800

    侧边栏配置前

---
+++ kanme/kanme/settings/develop.py
@@ -0,0 +1,15 @@
+from .base import *
+
+
+DEBUG = True
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'kanme',
+        'HOST': '127.0.0.1',
+        'USER': 'root',
+        'PORT': '3306',
+        'PASSWORD': 'C5O0yfjH&Q*0',
+    }
+}
